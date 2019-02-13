be3133842c963eacbba236edefcd901701ca22cb
adriancarlos <47173768+adriancarlos@users.noreply.github.com>
2019-02-13 09:47:30 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.128.32.206');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
    define('DB_DATABASE', 'employees');
